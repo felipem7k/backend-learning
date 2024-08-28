f93ee3a96ab902d95c56ec97ebe53daa4bbec8b7
Felipe <felipems3004@gmail.com>
2024-08-28 19:04:03 -0300

    Organização: cursos separados em pastas por módulo

---
+++ fundamentals/modulo-2/aula-5/desafios/app.js
@@ -0,0 +1,20 @@
+// Desafio 1
+let listaGenerica = [];
+
+// Desafio 2
+let linguagensDeProgramacao = ['JavaScript','C','C++','Kotlin','Python'];
+
+// Desafio 3
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+// Desafio 4
+let nomes = ['Pedro','Ana','Gustavo'];
+console.log(nomes[0]);
+
+// Desafio 5
+console.log(nomes[1]);
+
+// Desafio 6 
+console.log(nomes[nomes.length - 1]);
