4d3fef7457bc28334692f8aff5f42337d5d0ed7e
felipem7k <felipems3004@gmail.com>
2026-07-15 12:09:28 -0300

    criada classe eletronico, heranca de produto e com garantia

--- javascript/sctec-introducao-a-backend/desafio-poo/app.js
+++ javascript/sctec-introducao-a-backend/desafio-poo/app.js
@@ -9,5 +9,16 @@ class Produto {
     }
 }
 
+class Eletronico extends Produto {
+    constructor(nome, preco, garantia) {
+        super(nome, preco);
+        
+        this.garantia = garantia;
+    }
+}
+
 const produto = new Produto("Celular", 2000);
 produto.mostrarDetalhes();
+
+const eletronico = new Eletronico("Notebook", 3000, true);
+console.log(eletronico);
