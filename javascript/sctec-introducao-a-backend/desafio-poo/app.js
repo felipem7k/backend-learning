8e70068ce4a33acebe0b368ab5ceba4db327a34b
felipem7k <felipems3004@gmail.com>
2026-07-15 12:11:21 -0300

    garantia incluida ao mostrarDetalhes

--- javascript/sctec-introducao-a-backend/desafio-poo/app.js
+++ javascript/sctec-introducao-a-backend/desafio-poo/app.js
@@ -15,10 +15,14 @@ class Eletronico extends Produto {
         
         this.garantia = garantia;
     }
+
+    mostrarDetalhes() {
+        console.log(`${this.nome} | R$ ${this.preco} | ${this.garantia ? "possui" : "não possui"} garantia`);
+    }
 }
 
 const produto = new Produto("Celular", 2000);
 produto.mostrarDetalhes();
 
-const eletronico = new Eletronico("Notebook", 3000, true);
-console.log(eletronico);
+const eletronico = new Eletronico("Notebook", 3000, false);
+eletronico.mostrarDetalhes();
