49c04e9cadae31c5a8b2f271735720a1768b2245
felipem7k <felipems3004@gmail.com>
2026-07-17 19:24:54 -0300

    export da calculadora com funoes utilitarias

---
+++ javascript/sctec-introducao-a-backend/desafio-modulo/models/Calculadora.js
@@ -0,0 +1,17 @@
+export default {
+    somar: function(valor1, valor2) {
+        return valor1 + valor2;
+    },
+
+    subtrair: function(valor1, valor2) {
+        return valor1 - valor2;
+    },
+    
+    multiplicar: function(valor1, valor2) {
+        return valor1 * valor2;
+    },
+
+    dividir: function(valor1, valor2) {
+        return valor1 / valor2;
+    }
+}
