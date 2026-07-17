d84cea2d8e7f40646c703ec0e92b3e67cf72f855
felipem7k <felipems3004@gmail.com>
2026-07-17 20:40:51 -0300

    rotas em arquivo separado SRP

--- javascript/sctec-introducao-a-backend/aula-srp/src/app.ts
+++ javascript/sctec-introducao-a-backend/aula-srp/src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import express from "express";
+import clientsRouter from "./routes/clients.js"; 
+const app = express();
+
+app.use(clientsRouter);
+
+app.listen(3000, () => {
+    console.log("Ouvindo em http://localhost:3000");
+});
