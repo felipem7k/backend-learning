ab2d18385c8fe7ac0d3145ca5f327b8474917f1c
felipem7k <felipems3004@gmail.com>
2026-07-20 15:48:13 -0300

    feat: adicionado dotenv config + PORT

--- javascript/sctec-introducao-a-backend/projeto-estudos/src/app.ts
+++ javascript/sctec-introducao-a-backend/projeto-estudos/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import clientsRouter from "./routes/clients.js"; 
 const app = express();
+import { config } from "dotenv-safe";
+
+config();
 
 app.set("view engine", "pug");
 app.set("views", "./src/views");
 
 app.use(clientsRouter);
 
-app.listen(3000, () => {
-    console.log("Ouvindo em http://localhost:3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Ouvindo em http://localhost:${process.env.PORT}`);
 });
