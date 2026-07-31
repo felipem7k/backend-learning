25f5a97ee86eec09ac6362d80ba3dc4c16e266f0
felipem7k <felipems3004@gmail.com>
2026-07-31 12:45:31 -0300

    feat: create chirp store method

--- php/laravel/laravel-course/chirper/app/Http/Controllers/ChirpController.php
+++ php/laravel/laravel-course/chirper/app/Http/Controllers/ChirpController.php
@@ -35,7 +35,16 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null
+        ]);
+
+        return redirect('/')->with('success', 'Chirp created!');
     }
 
     /**
