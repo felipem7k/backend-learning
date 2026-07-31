a36131c1dd9f762b2f742ed13325ad8a2c024acb
felipem7k <felipems3004@gmail.com>
2026-07-31 13:42:12 -0300

    chore: chip delete controller

--- php/laravel/laravel-course/chirper/app/Http/Controllers/ChirpController.php
+++ php/laravel/laravel-course/chirper/app/Http/Controllers/ChirpController.php
@@ -91,8 +91,10 @@ class ChirpController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Chirp $chirp)
     {
-        //
+        $chirp->delete();
+
+        return redirect('/')->with('success', 'Your chip has been deleted!');
     }
 }
