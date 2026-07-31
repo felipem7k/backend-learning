5b3759b9bf864827aa212677686d34191c49a144
felipem7k <felipems3004@gmail.com>
2026-07-31 13:02:39 -0300

    chore: customize chirps message

--- php/laravel/laravel-course/chirper/app/Http/Controllers/ChirpController.php
+++ php/laravel/laravel-course/chirper/app/Http/Controllers/ChirpController.php
@@ -36,7 +36,10 @@ class ChirpController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'message' => 'required|string|max:255'
+            'message' => 'required|string|max:255',
+        ], [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
         ]);
 
         Chirp::create([
