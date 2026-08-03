bf2fef0137950f059e887cf516dd6de99ad52ae3
felipem7k <felipems3004@gmail.com>
2026-08-03 11:38:38 -0300

    fix: wrong login redirect

--- php/laravel/laravel-course/chirper/resources/views/components/layout.blade.php
+++ php/laravel/laravel-course/chirper/resources/views/components/layout.blade.php
@@ -15,10 +15,10 @@
 <body class="min-h-screen flex flex-col bg-base-200 font-sans">
     <nav class="navbar bg-base-100">
         <div class="navbar-start">
-            <a href="/login" class="btn btn-ghost text-xl">🐦 Chirper</a>
+            <a href="#" class="btn btn-ghost text-xl">🐦 Chirper</a>
         </div>
         <div class="navbar-end gap-2">
-            <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
+            <a href="/login" class="btn btn-ghost btn-sm">Sign In</a>
             <a href="/register" class="btn btn-primary btn-sm">Sign Up</a>
         </div>
     </nav>
