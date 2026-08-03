49c127683eae88943977b23894263eb199349195
felipem7k <felipems3004@gmail.com>
2026-08-03 11:15:58 -0300

    feat: create register route

--- php/laravel/laravel-course/chirper/routes/web.php
+++ php/laravel/laravel-course/chirper/routes/web.php
@@ -8,3 +8,5 @@ Route::post('/chirps', [ChirpController::class, 'store']);
 Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+Route::view('/register', 'auth.register');
