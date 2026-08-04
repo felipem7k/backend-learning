ac05e09ff969b76b11b0e45a944e0a24f7cb28a3
felipem7k <felipems3004@gmail.com>
2026-08-04 09:58:18 -0300

    chore: create login routes

--- php/laravel/laravel-course/chirper/routes/web.php
+++ php/laravel/laravel-course/chirper/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
@@ -19,5 +20,12 @@ Route::view('/register', 'auth.register')
     ->name('register');
 Route::post('/register', Register::class)
     ->middleware('guest');
+
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
 Route::post('/logout', Logout::class)
     ->middleware('auth');
